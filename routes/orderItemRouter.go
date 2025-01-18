c08dd18ae0f4287be82f5e86cc5c47278c8d9216
Ian Andwati <andwatiian@gmail.com>
2025-01-18 23:07:08 +0300

    completed routes

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "restaurant-api/controllers"
+)
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/order_items", controller.GetOrderItems())
+	incomingRoutes.GET("/order_items/:order_items_id", controller.GetOrderItem())
+	incomingRoutes.POST("/order_items", controller.CreateOrderItem())
+	incomingRoutes.PATCH("/order_items/:orde_items_id", controller.UpdateOrderItem())
+}
