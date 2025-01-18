ae94f10be221dfa9f960d12e1e655c83ed806e17
Ian Andwati <andwatiian@gmail.com>
2025-01-18 22:36:54 +0300

    initialized main.go

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+	"restaurant-api/database"
+	"restaurant-api/middleware"
+	"restaurant-api/routes"
+)
+
+var foodCollection  *.mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
